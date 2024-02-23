a1336dbf3ac222664c87cc68261d6fa14aa377cb
johncornelles <john.cornellews@kalvium.community>
2024-02-23 10:19:30 +0530

    crud operation complete

--- server.js
+++ server.js
@@ -1,23 +1,37 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const { connectToDB } = require("./config/DB")
-const connectTodb = async () => {
+const express = require('express');
+const app = express();
+const bodyParser = require("body-parser")
+const port = 3000;
+const { connectToDB } = require("./config/DB");
+const { getData, postData, updateData, deleteData } = require('./routes/routes');
+
+// Middleware to parse JSON requests
+app.use(express.json());
+app.use(bodyParser.json())
+
+// Routes
+app.use("/", getData);
+app.use("/", postData);
+app.use("/", updateData)
+app.use("/", deleteData)
+// Connect to the database and start the server
+const connectToDbAndStartServer = async () => {
     try {
-        await connectToDB()
+        await connectToDB();
         app.listen(port, () => console.log("Server is being listened on", port));
-    }
-    catch (err) {
-        console.error("AN error has occured", err)
+    } catch (err) {
+        console.error("An error has occurred", err);
     }
 }
 
-connectTodb()
-app.use(express.json())
+connectToDbAndStartServer();
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+    res.send('Hello World!');
+});
 
 app.get('/ping', (req, res) => {
-    res.json({ messsage: "ping is active" })
-})
+    res.json({ message: "Ping is active" });
+});
+
+module.exports = app;
